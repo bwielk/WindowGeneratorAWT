2f258bcb4818a43bde586c1738e6394851c62e48
bwielk <bwielk@gmail.com>
2018-03-01 21:21:01 +0100

    Window gets generated

--- src/com/example/awtSample/Main.java
+++ src/com/example/awtSample/Main.java
@@ -1,5 +1,9 @@
 package com.example.awtSample;
 
 public class Main {
-
+	
+	public static void main(String[] args){
+		MyWindow myWindow = new MyWindow("Sample text");
+		myWindow.setVisible(true);
+	}
 }
